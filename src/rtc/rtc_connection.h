f8fccdb51718114d01bdcedf38868abf98d51d7b
sumi-ryosuke <sumi-ryosuke@suminoMacBook-Air.local>
2025-08-14 23:40:28 +0900

    remove track

--- src/rtc/rtc_connection.h
+++ src/rtc/rtc_connection.h
@@ -41,6 +41,7 @@ class RTCConnection {
   bool SetVideoEnabled(bool enabled);
   bool IsAudioEnabled();
   bool IsVideoEnabled();
+  void RemoveRemoteTrack(const std::string& track_id);
 
   void GetStats(std::function<void(const webrtc::scoped_refptr<
                                    const webrtc::RTCStatsReport>&)> callback);
