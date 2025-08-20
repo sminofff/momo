368f831efe4a9feefb2afa54ac0a88dc650aa8d1
sumi-ryosuke <sumi-ryosuke@suminoMacBook-Air.local>
2025-08-20 21:33:33 +0900

    add pion sub option for codecs

--- src/momo_args.h
+++ src/momo_args.h
@@ -85,6 +85,7 @@ struct MomoArgs {
   std::string ayame_signaling_key = "";
 
   std::string pion_signaling_url;
+  std::string pion_video_codec_type = "";  // VP8, VP9, H264, H265, AV1, ALL, or empty for H264 default
 
   bool disable_echo_cancellation = false;
   bool disable_auto_gain_control = false;
